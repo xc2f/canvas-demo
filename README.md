6797b6408f49d386ea30e1555dce1411891f4306
Dachow <hellowd93@163.com>
2017-02-09 02:10:16 +0800

    begin client demo

--- README.md
+++ README.md
@@ -18,4 +18,6 @@ Visit on [GitHub Pages](https://dachow.github.io/canvas-demo/)
     
   > 参考自慕课网liuyubobobo老师的[课程](http://www.imooc.com/learn/133)。与教学demo不同在于改造为计时程序，并可打印分段计时时间。 `v-s关系`
 
+  - [client](https://Dachow.github.io/canvas-demo/client/)
 
+  > 终端界面绘制，并模拟在终端上的操作，实现基本的交互动画。进行中……
---
+++ client/mobile.js
@@ -0,0 +1,19 @@
+﻿var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+canvas.width = 1280;
+canvas.height = 720;
+
+
+drawEdge(context, canvas.width / 2, canvas.height / 2, 200, 300);
+
+
+function drawEdge(ctx, x, y, width, height) {
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.scale(width, height);
+    ctx.beginPath();
+    ctx.moveTo(-1, -1);
+    ctx.lineTo(1, -1);
+    ctx.restore();
+    ctx.stroke();
+}
