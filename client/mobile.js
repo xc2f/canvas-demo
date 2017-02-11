96dcc4a55d27a53225c1a499094e5c6524c044c0
Dachow <hellowd93@163.com>
2017-02-12 01:38:28 +0800

    edge okay

--- client/mobile.js
+++ client/mobile.js
@@ -6,7 +6,7 @@ canvas.height = 720;
 //画网格
 drawGrid(context, canvas.width / 2, canvas.height / 2, canvas.width, canvas.height, 10);
 //轮廓
-drawEdge(context, canvas.width / 2, canvas.height / 2, 400, 670, 10);
+drawEdge(context, canvas.width / 2, canvas.height / 2, 370, 670, 27);
 
 //未完待续
 drawWWDX(context, canvas.width - 70, canvas.height - 100);
@@ -15,13 +15,13 @@ function drawWWDX(ctx, x, y) {
     ctx.textAlign = 'right';
     ctx.fillText('未完待续', x, y);
     ctx.fillText('Unfinished', x, y + 30);
-    ctx.fillText('2017.2.10', x, y + 30 + 30);
+    ctx.fillText('2017.2.12', x, y + 30 + 30);
 }
 
 
 function drawEdge(ctx, x, y, width, height, r, l_r, t_b, btn_w, btn_h) {
     r = r || 10;
-    l_r = l_r || 12;
+    l_r = l_r || 7;
     t_b = t_b || 55;
 
     ctx.save();
@@ -50,11 +50,11 @@ function drawEdge(ctx, x, y, width, height, r, l_r, t_b, btn_w, btn_h) {
 
 
     //logo
-    ctx.font = 'bold 27px Arial';
+    ctx.font = 'bold 17px Arial';
     ctx.textAlign = 'center';
-    ctx.textBaseline = 'bottom';
-    ctx.fillStyle = '#777';
-    ctx.fillText('Dachow', 0, -height / 2 + t_b);
+    //ctx.textBaseline = 'bottom';
+    ctx.fillStyle = '#c0c0c0';
+    ctx.fillText('DACHOW', 0, -height / 2 + t_b - t_b * 1 / 5);
 
     // 底部按钮
     ctx.beginPath();
@@ -68,11 +68,74 @@ function drawEdge(ctx, x, y, width, height, r, l_r, t_b, btn_w, btn_h) {
     ctx.lineTo(-btn_w / 2, height / 2 - t_b / 2 + btn_h / 2);
     ctx.arc(-btn_w / 2, height / 2 - t_b / 2, btn_h / 2, Math.PI * 1 / 2, Math.PI * 3 / 2);
     ctx.closePath();
+    ctx.lineWidth = 2;
     ctx.stroke();
 
 
+    //还原上面lineWidth = 2
+    ctx.lineWidth = 1;
+
+    //左侧两按钮
+    sideBtn(ctx, -width, height, height / 3);
+    sideBtn(ctx, -width, height, height / 4);
+
+    //右侧按钮
+    sideBtn(ctx, width, height, height / 5);
+
+
+    //听筒，长度为底部按钮长度btn_w*1.5, 高度为1/3*btn_h，半径为btn_h * 1 / 3 / 2
+    ctx.beginPath();
+    btn_w *= 1.5;
+    //右半圆弧
+    ctx.arc(btn_w / 2, -height / 2 + t_b / 4, btn_h * 1 / 3 / 2, Math.PI * 3 / 2, Math.PI * 1 / 2);
+    ctx.lineTo(-btn_w / 2, -height / 2 + t_b / 4 + btn_h * 1 / 3 / 2)
+    ctx.arc(-btn_w / 2, -height / 2 + t_b / 4, btn_h * 1 / 3 / 2, Math.PI * 1 / 2, Math.PI * 3 / 2);
+    ctx.closePath();
+    ctx.stroke();
+
+    //听筒内部扬声器小点，距左侧为5，递增为7，半径为1
+    for (var i = 5; i < btn_w; i += 7) {
+        //防止出现上一步递增后的值距btn_w小于5的情况，即本次绘制的点过分靠右（间距小于5）
+        if (i + 5 <= btn_w) {
+            ctx.beginPath();
+            ctx.arc(-btn_w / 2 + i, -height / 2 + t_b / 4, 1, 0, Math.PI * 2);
+            ctx.stroke();
+        }
+    }
+
+    //还原btn_w的值
+    btn_w /= 1.5;
+
+    //感光圈，距左侧一半宽度的1/4处；高度与扬声器一致；半径为5
+    //两个光圈相聚为1个r，即第二个相对于第一个偏移3个r
+    ctx.beginPath();
+    ctx.arc(-width / 2 + width * 1 / 4, -height / 2 + t_b / 4, 5, 0, Math.PI * 2);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(-width / 2 + width * 1 / 4 + 3 * 5, -height / 2 + t_b / 4, 5, 0, Math.PI * 2);
+    ctx.fill();
+
+    //前摄像头，距左侧一半宽度的3/4处，半径为7
+    ctx.beginPath();
+    ctx.arc(-width / 2 + width * 3 / 4, -height / 2 + t_b / 4, 7, 0, Math.PI * 2);
+    ctx.fill();
+
     ctx.restore();
 
+}
+
+
+//侧部按钮，高为1/15的机身长度，突出部分为1/70机身宽度
+function sideBtn(ctx, width, height, btn_l) {
+    ctx.beginPath();
+    //从按钮上侧紧挨机身的部分绘制
+    ctx.moveTo(width / 2, -btn_l);
+    ctx.lineTo(width / 2 + width / 70, -btn_l);
+    ctx.lineTo(width / 2 + width / 70, -btn_l + height / 15);
+    ctx.lineTo(width / 2, -btn_l + height / 15);
+    ctx.closePath();
+    ctx.stroke();
 }
 
 
@@ -101,3 +164,22 @@ function drawGrid(ctx, x, y, width, height, size) {
     ctx.stroke();
     ctx.restore();
 }
+
+
+
+//获取鼠标指针坐标
+function getMousePos(evt, canvas) {
+    var rect = document.getElementById('canvas').getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+//显示锁屏
+canvas.addEventListener('click', function (evt) {
+    var mousePos = getMousePos(evt);
+    if (context.isPointInPath(mousePos.x, mousePos.y)) {
+        //重绘
+    }
+}, false);
